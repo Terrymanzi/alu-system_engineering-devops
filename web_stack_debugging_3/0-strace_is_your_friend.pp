7b256d5a1c4fe020cdd73de6788aae0f9f28da64
terrymanzi <m.terry@alustudent.com>
2024-07-29 08:38:27 -0700

    debugging task 0

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,9 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin'],
+# 0-strace_is_your_friend.pp
+file { '/var/www/html/index.php':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  content => "<?php\n// Default index page\n?>",
 }
-
